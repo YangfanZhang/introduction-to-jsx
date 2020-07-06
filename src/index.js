7f781306dd2d79face7c8a729d6419d779a15b2d
Yangfan Zhang <42492049+YangfanZhang@users.noreply.github.com>
2020-07-06 11:41:25 +1000

    Added react, reactDOM.render

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//1, what to show, 2 where to show, 3when to complete
+ReactDOM.render(
+  <div>
+    <h1> Hello</h1>
+  </div>,
+  document.getElementById("root")
+);
